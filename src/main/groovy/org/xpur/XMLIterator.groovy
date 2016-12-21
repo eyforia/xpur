9e5f83ecc5e6794af43208f901d5ce7baace2fa5
Andrei Karneyenka <eyforia@gmail.com>
2016-12-21 13:21:19 -0500

    changed to pure StAX implementation

--- src/main/groovy/org/xpur/XMLIterator.groovy
+++ src/main/groovy/org/xpur/XMLIterator.groovy
@@ -1,34 +1,92 @@
 package org.xpur
 
-import com.thoughtworks.xstream.XStream
-import com.thoughtworks.xstream.io.xml.StaxDriver
 import groovy.transform.CompileStatic
-import org.xpur.impl.ObjectInputStreamIterator
-import org.xpur.impl.StringMapConverter;
+
+import javax.xml.stream.XMLEventReader
+import javax.xml.stream.XMLInputFactory
+import javax.xml.stream.events.Attribute
+import javax.xml.stream.events.EndElement
+import javax.xml.stream.events.StartElement
+import javax.xml.stream.events.XMLEvent
 
 @CompileStatic
-public class XMLIterator {
-    private String elementName
+class XMLIterator implements Iterable<Map<String, Object>>, Iterator<Map<String, Object>> {
 
-    XMLIterator(String elementName) {
+    private XMLEventReader reader
+    private String elementName
+    private Map<String, Object> currentElement
+
+    XMLIterator(InputStream inputStream, String elementName) {
+        XMLInputFactory factory = XMLInputFactory.newInstance()
+        this.reader = factory.createXMLEventReader(inputStream)
         this.elementName = elementName
     }
 
-    Iterator<Map> iterate(InputStream is) {
-        ObjectInputStream ois = createParser(elementName).createObjectInputStream(is)
-        return new ObjectInputStreamIterator<Map>(ois)
+    XMLIterator(Reader reader, String elementName) {
+        XMLInputFactory factory = XMLInputFactory.newInstance()
+        this.reader = factory.createXMLEventReader(reader)
+        this.elementName = elementName
     }
 
-    Iterator<Map> iterate(Reader reader) {
-        ObjectInputStream ois = createParser(elementName).createObjectInputStream(reader)
-        return new ObjectInputStreamIterator<Map>(ois)
+    @Override
+    Iterator<Map> iterator() {
+        return this
     }
 
+    @Override
+    boolean hasNext() {
+        if (currentElement == null && reader.hasNext())
+            currentElement = findNext(elementName)
 
-    private XStream createParser(String elementName) {
-        XStream result = new XStream(new StaxDriver())
-        result.aliasType(elementName, Map)
-        result.registerConverter(new StringMapConverter())
+        return currentElement != null
+    }
+
+    @Override
+    Map<String, Object> next() {
+        Map<String, Object> result = currentElement
+        currentElement = null
+        return result
+    }
+
+    private Map<String, Object> findNext(String elementName) {
+        while (reader.hasNext()) {
+            XMLEvent event = reader.nextEvent()
+            if (event.isStartElement()) {
+                StartElement startElement = event.asStartElement()
+                if (startElement.name?.localPart == elementName) {
+                    return create(startElement)
+                }
+            }
+        }
+        return null
+    }
+
+    private Map<String, Object> create(StartElement startElement) {
+        Map<String, Object> result = new LinkedHashMap<>()
+        //add attributes first
+        Iterator<Attribute> attributes = (Iterator<Attribute>) startElement.attributes
+        attributes?.each { Attribute attr -> result.put(attr.name.localPart, attr.value) }
+
+        while (reader.hasNext()) {
+            XMLEvent event = reader.nextEvent()
+            if (event.isStartElement()) {
+                StartElement element = event.asStartElement()
+                XMLEvent next = reader.peek()
+                if (next.characters && !next.asCharacters().whiteSpace) {   //no nested tags
+                    result.put(element.name.localPart, reader.getElementText())
+                } else { //has nested tags
+                    result.put(element.name.localPart, create(element))
+                }
+            }
+
+            if (event.isEndElement()) {
+                EndElement endElement = event.asEndElement()
+                if (endElement.name == startElement.name) {
+                    break
+                }
+
+            }
+        }
         return result
     }
 
--- src/test/groovy/org/xpur/XMLIteratorTest.groovy
+++ src/test/groovy/org/xpur/XMLIteratorTest.groovy
@@ -2,35 +2,48 @@ package org.xpur
 
 import spock.lang.Specification
 
+import static java.nio.charset.StandardCharsets.UTF_8
+
 class XMLIteratorTest extends Specification {
+
+    List<Map<String, Object>> carsRefData = [
+            [
+                    id   : '1',
+                    make : 'Ford',
+                    model: 'Mustang GT',
+                    year : '2016'
+            ],
+            [
+                    id    : '2',
+                    make  : 'Honda',
+                    model : 'Accord',
+                    year  : '2015',
+                    dealer: [
+                            id   : '345',
+                            name : 'MyAutos',
+                            phone: '123-456-7890'
+                    ]
+            ]
+    ]
 
     def "iterate stream - happy path"() {
         def resource = getClass().getResourceAsStream("cars.xml")
 
         when:
-        def iterator = new XMLIterator("car")
-        def result = iterator.iterate(resource)?.collect { it }
+        def result = new XMLIterator(resource, "car").collect { it }
 
         then:
-        result == [
-                [
-                        id   : '1',
-                        make : 'Ford',
-                        model: 'Mustang GT',
-                        year : '2016'
-                ],
-                [
-                        id    : '2',
-                        make  : 'Honda',
-                        model : 'Accord',
-                        year  : '2015',
-                        dealer: [
-                                name : 'MyAutos',
-                                phone: '123-456-7890'
-                        ]
-                ]
-        ]
+        result == carsRefData
 
     }
 
+    def "iterate reader - happy path"() {
+        def reader = new InputStreamReader(getClass().getResourceAsStream("cars.xml"), UTF_8)
+
+        when:
+        def result = new XMLIterator(reader, "car").collect { it }
+
+        then:
+        result == carsRefData
+    }
 }
